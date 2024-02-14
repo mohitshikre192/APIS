234de24029b78f057f8c5218c30d769dcb0c9e75
mohitshikre192 <mohitshikre192@gmail.com>
2024-02-14 15:34:46 +0530

    database configuration applied

--- Models/Context.cs
+++ Models/Context.cs
@@ -2,8 +2,15 @@
 
 namespace APIS.Models
 {
-    public class Context:DbContext
+    public class Context : DbContext
     {
+        public Context() : base() { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Vehicle> Vehicles {get;set;}
+        public DbSet<Route> Routes {get;set;}
+        public DbSet<Driver> Drivers {get;set;}
 
     }
+
 }
